096d3c197996e2a0ad88efddbbf7abda0dd0af78
mahamnoorafzal <mahamnoor1408@gmail.com>
2026-09-24 12:28:57 +0500

    Fix output statement in HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,6 @@
 public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
-        System.out.println("Jenkins successfully ran my Java application.");
+        System.out.println("Jenkins successfully ran my Java application.")
     }
 }
